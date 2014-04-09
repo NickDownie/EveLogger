9749d4002be761902e9d3a97841c5355c79c3e33
NickDownie <nick.j.downie@gmail.com>
2014-04-09 00:06:03 -0400

    Added Array

--- EveAmarrShips/src/com/example/eveamarrships/MainActivity.java
+++ EveAmarrShips/src/com/example/eveamarrships/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.eveamarrships;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.app.ListActivity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		String[] ships={"Punisher", "Coercer", "Omen", "Bestower", "Legion", "Harbinger", "Abaddon", "Avatar"};
+		setListAdapter(new ArrayAdapter<String>(this,
+		R.layout.activity_main, R.id.space, ships));
+	}
+	protected void onListItemClick(ListView l, View v, int) {
+		
+		
 	}
 
 	@Override
